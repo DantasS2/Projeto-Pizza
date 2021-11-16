2a3ed7566b1341453980b04d8ee83c2daf6086f1
DantasS2 <94460910+DantasS2@users.noreply.github.com>
2021-11-16 10:42:36 -0300

    Create pizza.php

---
+++ pizza.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+<metacharset='utf-8'>
+<metahttp-equiv='X-UA-Compatible' content='IE=edge'>
+
+<title>Page Title</title>
+
+<metaname='viewport' content='width=device-width, initial-scale=1'>
+<!-- CSS only -->
+
+<linkhref="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- JavaScript Bundle with Popper -->
+
+<scriptsrc="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<divclass="accordion" id="#accordionExample">
+
+<divclass="accordion-item">
+
+<h2class="accordion-header" id="headingOne">
+
+<buttonclass="accordion-button"
+
+type="button"
+
+data-bs-toggle="collapse"
+
+data-bs-target="#collapseOne"
+
+aria-expanded="true"
+
+aria-controls="collapseOne">
+
+Pizza Calabresa
+
+</button>
+
+</h2>
+
+<divid="collapseOne"
+
+class="accordion-collapse collapse show"
+
+aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+
+<divclass="accordion-body">
+
+<strong>Ingredientes:</strong> Calabresa fatiada, cebola e queijo.
+
+</div>
+
+</div>
+
+</div>
+
+<divclass="accordion-item">
+
+<h2class="accordion-header" id="headingcheddar">
+
+<buttonclass="accordion-button"
+
+type="button"
+
+data-bs-toggle="collapse"
+
+data-bs-target="#collapsecheddar"
+
+aria-expanded="true"
+
+aria-controls="collapsecheddar">
+
+Pizza de queijo e bacon
+
+</button>
+
+</h2>
+
+<divid="collapsecheddar"
+
+class="accordion-collapse collapse "
+
+aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+
+<divclass="accordion-body">
+
+<strong>Ingredientes:</strong> Queijo cheddar e bacon.
+
+</div>
+
+</div>
+
+</div>
+
+
+<divclass="accordion-item">
+
+<h2class="accordion-header" id="headingbrocolis">
+
+<buttonclass="accordion-button"
+
+type="button"
+
+data-bs-toggle="collapse"
+
+data-bs-target="#collapsebrocolis"
+
+aria-expanded="true"
+
+aria-controls="collapsebrocolis">
+
+Pizza de queijo e brócolis
+
+</button>
+
+</h2>
+
+<divid="collapsebrocolis"
+
+class="accordion-collapse collapse "
+
+aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+
+<divclass="accordion-body">
+
+<strong>Ingredientes:</strong> Queijo mussarela e brócolis.
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
